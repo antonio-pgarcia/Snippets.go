0335292b83e9e7f9fd7bd68c417a8d368067cf24
Antonio Prestes García <antonio.garcia2@gmail.com>
2014-12-10 17:36:47 +0100

    Update ListShuffle.go

--- ListShuffle.go
+++ ListShuffle.go
@@ -1,42 +1,65 @@
 package main
 
-import(
-    "fmt"
-    "math/rand"
+import (
+	"fmt"
+	"math/rand"
 )
 
 // -- | Append two slices
 func myappend(s1, s2 []int) []int {
 	for i := range s2 {
-		s1= append(s1, s2[i])
+		s1 = append(s1, s2[i])
 	}
-	return s1	
+	return s1
 }
 
 // -- | Removes the the nth element from list
-func drop(i int, l []int) [] int {
-	return myappend(l[:i],l[i+1:])
+func drop(i int, l []int) []int {
+	return myappend(l[:i], l[i+1:])
 }
 
 // -- | Pop one element from a specific list position
 func pop(i int, l []int) (int, []int) {
-	return l[i], l
+	var e = l[i]
+	return e, drop(i, l)
 }
 
 // -- | Generate random number within interval a-b with seed s
 func myrandom(a, b, s int) int {
-    rand.Seed(int64(s))
-    return rand.Intn(b - a) + a
+	fmt.Println(a, b, s)
+	if a == 0 && b == 0 {
+		return 0
+	}
+	rand.Seed(int64(s))
+	return rand.Intn(b-a) + a
+}
+
+// -- | Shuffle a list (golang slice)
+func shuffle(l []int) []int {
+	return shufflep(myrandom(0, len(l)-1, 0), l, []int{})
 }
 
 // -- | Shuffle' auxiliary
-func shufflep() {
+func shufflep(r int, l1, l2 []int) []int {
+	//fmt.Println(len(l1))
+	var llen = len(l1)
+	h, t := pop(r, l1)
+	fmt.Println(h, t)
+	//fmt.Println(l2)
+	if llen == 0 {
+		return l2
+	} else {
+		//return l1
+		shufflep(myrandom(0, llen-1, r), t, append(l2, h))
+	}
+	return l2
 }
 
 // -- | The main entry point.
 func main() {
-	var l= []int {1,2,3,4,5,6,7,8,9,10}
+	var l = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	//var v= []int {}
 	fmt.Println("List Shuffle!")
-	//fmt.Println(myappend(l,l[:i]))
-	fmt.Println(drop(4,l))
+	//fmt.Println(len(v))
+	fmt.Println(shuffle(l))
 }
